01bc6b036b752ba884ca57e978edd1aeadca00e2
Datchthana1 <140144536+Datchthana1@users.noreply.github.com>
2025-09-10 21:18:13 +0700

    import picture for header

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,13 @@ import Navbar from "@/components/Navbars";
 import Footer from "../components/Footer";
 import "./globals.css";
 
+export const metadata = {
+  title: "My Resume Web",
+  icons: {
+    icon: "/profile.png", // path จาก public
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
