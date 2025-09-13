bfcb3efc745b1e36c3622d71d5c02d16c925fca4
Datchthana1 <140144536+Datchthana1@users.noreply.github.com>
2025-09-13 10:35:45 +0700

    EditNavbarSpacing

--- app/Experience/page.jsx
+++ app/Experience/page.jsx
@@ -59,7 +59,7 @@ const Page = () => {
 
   return (
     <div className="font-sans min-h-screen bg-gray-100 text-gray-900 px-5 pt-5 space-y-6 pb-5 min-w-sc">
-      <div className="max-w-5xl mx-auto px-6 py-6 animate-fadeInUp">
+      <div className="max-w-5xl mx-auto px-6 animate-fadeInUp">
         {Object.entries(Experience).map(([title, details], index) => (
           <div
             key={index}
