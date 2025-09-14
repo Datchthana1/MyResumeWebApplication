1088443f73120e38308c8f366d423a68a37aada5
Datchthana1 <140144536+Datchthana1@users.noreply.github.com>
2025-09-14 11:25:24 +0700

    editedCard

--- components/ExperienceList.jsx
+++ components/ExperienceList.jsx
@@ -7,8 +7,8 @@ export default function ExperienceList({
   setClosing,
 }) {
   return (
-    <div className="font-sans min-h-screen bg-gray-100 text-gray-900 px-5 pt-5 space-y-6 min-w-sc">
-      <div className="max-w-5xl mx-auto px-6 animate-fadeInUp">
+    <div className="font-sans min-h-screen bg-gray-100 text-gray-900 pt-5 space-y-6 min-w-sc">
+      <div className="max-w-5xl mx-auto animate-fadeInUp">
         {Object.entries(Experience).map(([title, details], index) => (
           <ExperienceCard
             key={index}
